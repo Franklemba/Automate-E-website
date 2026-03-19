b3a0835bbf1d8bd586663a974a59c816582aa059
Lembalemba <franklembalemba3@gmail.com>
2026-03-19 13:57:35 +0200

    updated the game version

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 
 const Game = () => (
-  <div className="fixed inset-0 bg-black">
+  <div className="relative bg-black min-h-[100dvh]">
     <iframe
       src="/automateEGame.html"
       title="AutomateE Automation Journey Game"
-      className="w-full h-full border-0"
+      className="w-full h-[100dvh] border-0"
       allow="clipboard-write"
     />
     <div className="absolute top-3 left-3 z-10">
